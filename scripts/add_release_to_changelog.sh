294b28e39f13899ae97d6c38bf90ce2e45e91ac2
Mikhail Kirillov <m.kirillov@postgrespro.ru>
2026-09-26 13:23:39 +0300

    chore: fix changelog script

--- scripts/add_release_to_changelog.sh
+++ scripts/add_release_to_changelog.sh
@@ -8,20 +8,16 @@ command -v git-cliff >/dev/null 2>&1 || { echo "Error: git-cliff is required."; 
 echo "work directory: $(pwd)"
 echo "check uncommited changes"
 
-git diff --quiet HEAD || { echo "found local chnages" ; exit 1; }
+git diff --quiet HEAD || { echo "found local changes" ; exit 1; }
 
 export NEXT_TAG=$(git cliff --bumped-version 2> /dev/null)
 
 echo "new release tag: $NEXT_TAG"
 
-echo "update change log"
+echo "update changelog"
 
 export TAG_CONTENT=$(git cliff --bump --unreleased --strip all)
 
 git cliff -u --tag $NEXT_TAG --prepend CHANGELOG.md
 git add CHANGELOG.md
 git commit -m "chore(release): prepare for $NEXT_TAG"
-
-git tag -a $NEXT_TAG -m "$TAG_CONTENT"
-
-git push origin $NEXT_TAG
